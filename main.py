a8727e0dd7b79bdd2452f30fb0a28198b7d0c91a
RafaelGuimaraes <rafaelfigueredog@gmail.com>
2018-09-08 01:34:43 -0300

    Etapas 1 e 2 concluídas

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 from grafo import Grafo
 
-try:
-    while(1):
-        N = input()
+def recebedados():
+    global vertices
+    vertices = input()
+    vertices = vertices.split(", ")
+    chave = True
+    for i in vertices:
+        if len(i) != 0:
+            for j in i:
+                if j >= chr(65) and j <= chr(90):
+                    continue
+                else:
+                    chave = False
+                    break
+        else:
+            chave = False
+            break
+
+    if (chave == False):
+        print("Digite novamente:")
+        vertices = recebedados()
+    
+    return vertices
+
+def main ():
+
+    vertices = []
+    arestas = []
+
+    vertices = recebedados()
+    g = Grafo()
+    for i in range(len(vertices)):
+        g.adicionaVertice(vertices[i])
+    print(g)
+
+    return 0
+    
+
+main()

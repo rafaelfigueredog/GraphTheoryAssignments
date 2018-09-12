186c1dab34636fbcc22ce76ad524fb4eb7dd0581
RafaelGuimaraes <rafaelfigueredog@gmail.com>
2018-09-11 22:40:32 -0300

    VerticesNaoAdjacentes Adc.

--- roteiro.py
+++ roteiro.py
@@ -1,6 +1,25 @@
 from grafo import Grafo
-from itertools import permutations
+from itertools import product
+
+
 
 def vertices_nao_adjacentes(g: Grafo):
-    preprocessamento = permutations(g.N)
-    print(g.N)
+    possibilidades = []
+    genArestas = product(g.N, repeat=2)
+    for subset in genArestas:
+        aresta =  subset[0] + "-" + subset[1]
+        possibilidades.append(aresta)
+    print(possibilidades)
+    naoAdjacentes = []
+    for i in possibilidades:
+        if (i not in g.A.values()):
+            naoAdjacentes.append(i)
+    return naoAdjacentes
+
+
+def main(): 
+    g = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'], {'a1':'J-C', 'a2':'C-E', 'a3':'C-E', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a7':'C-T', 'a8':'M-T', 'a9':'T-Z'})
+    print(g)
+    vertices_nao_adjacentes(g)
+
+main()

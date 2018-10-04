61180cb9fde5425476aaa507ef2edceff3e35727
RafaelGuimaraes <rafaelfigueredog@gmail.com>
2018-10-04 13:55:44 -0300

    roteiro2 modified

--- roteiro2.py
+++ roteiro2.py
@@ -32,8 +32,5 @@ pass
 
 def main():
 
-    g_p = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'], {'a1':'J-C', 'a2':'C-E', 'a3':'C-E', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a7':'C-T', 'a8':'M-T', 'a9':'T-Z'})
-    
-
     
 main()

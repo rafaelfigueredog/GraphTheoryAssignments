3b302a797df6536c2aa7fb533d12d32d62e6ccd6
Rafael Guimarães <rafaelfigueredog@gmail.com>
2018-11-06 11:45:25 -0300

    infty

--- Dikstra/dikstra.py
+++ Dikstra/dikstra.py
@@ -1,4 +1,36 @@
 from grafo_adj import Grafo
+from numpy import infty
 
-def Dikstra(g: Grafo, vi, vf, carga):
+
+
+def Dijkstra(g: Grafo, vi, vf):
+
+    Distancias = {}
+
+    for i in g.N:
+
+        if i == vi:
+            Distancias[str(i)] = 0
+        else:
+            Distancias[str(i)] = infty
     
+
+    
+
+
+
+
+
+def main():
+
+    g_p = Grafo([], [])
+    for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
+        g_p.adiciona_vertice(i)
+    for i in ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
+        g_p.adiciona_aresta(i) 
+
+
+    Dijkstra(g_p, 'C', 'Z')
+
+
+main()

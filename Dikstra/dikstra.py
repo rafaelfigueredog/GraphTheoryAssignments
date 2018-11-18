503b6417c9c0fd635749a937aaab92e433df0573
Rafael Guimarães <rafaelfigueredog@gmail.com>
2018-11-18 04:53:17 -0300

    Avanço no Passo 4

--- Dikstra/dikstra.py
+++ Dikstra/dikstra.py
@@ -1,38 +1,49 @@
 from grafo_adj import Grafo
-from dikstradic import dijkstra
 
-def Dijkstra(g: Grafo, vi, vf):
-
-    beta = [] # disancias
-    pi = [] # predecessor
-    phi = [] # temporario ou permanente
+def Dijkstra(g: Grafo, u, v):
 
     vertices = g.N
-    w = g.N.index[vi]
+    vertLength = len(vertices)
+    
+    # 1. 𝞫(r) ⇽ ∞, 𝞿(r) ⇽ 0, 𝞹(r) ⇽ 0 e w ⇽ u
+    # 2. 𝞫(u) ⇽ 0 e 𝞿(u) ⇽ 1
 
-    # 1. 𝞫(u) ⇽ 0 e 𝞿(u) ⇽ 1
-    # 2. 𝞫(r) ⇽ ∞, 𝞿(r) ⇽ 0, 𝞹(r) ⇽ 0 e w ⇽ u
+    w = g.N.index(u)
 
-    for i in range(len(g.N)):
-        if g.N[i] == vi:
-            w = i
-            beta.append(0)
-            phi.append(1)
-        else:
-            beta.append(float('inf'))
-            phi.append(0)
-        pi.append(0)
+    beta = vertLength*[ float("inf") ] # distancias
+    phi = vertLength*[0] # temporario ou permanente
+    pi = vertLength*[0] # predecessor
+
+    beta[w] = 0
+    phi[w] = 1
 
     # 3. 𝞿(r) = 0  𝞫(r) > 𝞫(w) + 𝞪(w,r)
+    while ( vertices[w] != v ):
 
-    for r in g.M[w]:
-        if g.M[w][r] != 0:
-            alfa_w_r = g.M[w][r] 
-            if ( phi[r] = 0 and  beta[r] > ( beta[w] + alfa_w_r ) ): 
-                # 𝞫(r) ⇽ 𝞫(w)+𝞪(w,r) 
-                # 𝞹(r) ⇽ w
-                beta[r] = beta[w] + alfa_w_r
-                phi[r] = vertices[w]
+        candidatoAsterisco = float('inf')
+        R = -1
+        for r in g.M[w]:
+            if g.M[w][r] != 0:
+                alfaWR = g.M[w][r]
+                if (phi[r] == 0 and beta[r] > (beta[w] + alfaWR)): # compara ultima distancia 
+                    
+                    # 𝞫(r) ⇽ 𝞫(w)+𝞪(w,r)
+                    # 𝞹(r) ⇽ w
+                    beta[r] = beta[w] + alfaWR
+                    pi[r] = vertices[w]
+
+                    # Ache um vértice r* tal que:
+                    # 𝞿(r*) = 0, 𝞫(r*)<∞ e 𝞫(r*)=min 𝞿(r) = 0 ( 𝞫(r) )
+                    if beta[r] < candidatoAsterisco:
+                        candidatoAsterisco = beta[r]
+                        R = r
+        if (R > 0):
+            phi[R] = 1
+            w = R
+    
+    
+    
+
 
 
 
@@ -41,8 +52,10 @@ def main():
     g_p = Grafo([], [])
     for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
         g_p.adiciona_vertice(i)
-    for i in ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
+    for i in ['J-C','J-C','J-E','C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
         g_p.adiciona_aresta(i) 
+
+    print(g_p)
 
     print( Dijkstra(g_p, 'J', 'Z')  )
 

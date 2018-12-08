214a5799cb8b7db2418e3155dcb77b064a3d8210
Rafael Guimarães <rafaelfigueredog@gmail.com>
2018-12-08 15:45:06 -0300

    start kruskall

--- Spanning Tree/Kruskall.py
+++ Spanning Tree/Kruskall.py
@@ -1,29 +1,8 @@
 from  grafo_adj_nao_dir import Grafo
 from PriorityQueue import PriorityQueue
 
-def Prim(g: Grafo):
+def kruskall(g: Grafo):
 
-    verticeInicial = 0
-    mst = Grafo() # mst = Minimum Spanning Tree
-    mst.adiciona_vertice(g.N[verticeInicial])
-    idx = verticeInicial
-
-    while (mst.quantidadeVertices != g.quantidadeVertices):
-
-        for i in range( idx, len(g.M[idx]) ):
-            if (g.M[idx][i] > 0): # encontrei um vertice fora da arvore
-                mst.adiciona_vertice(g.N[i]) # adiciono o vertice na arvore
-                aresta = g.N[verticeInicial] + '-' + g.N[i] # crio a aresta
-                tamanhoAresta = g.Mfila[idx][i].remove() # menor peso pela fila de prioridade
-                mst.adiciona_aresta(aresta, tamanhoAresta)
-
-            # fazer o algoritimo girar
-
-        """ idxfixo = idx
-        while ( (idx - 1) >= 0 ):
-            if ( g.M[idx-1][idx] != str ):
-                grau += g.M[idx-1][idxfixo]
-            idx -= 1 """
 
             
 def main():
--- Spanning Tree/prim.py
+++ Spanning Tree/prim.py
@@ -1,7 +1,6 @@
 from  grafo_adj_nao_dir import Grafo
 from PriorityQueue import PriorityQueue
 import graphLibrary
-from igraph import *
 import copy
 
 def Prim(g: Grafo):
@@ -17,72 +16,71 @@ def Prim(g: Grafo):
 
     # calcula o grau de todos os vertices  e coloca numa lista
     # com a mesma indexação dos vertices
-    licacoesValidas = [0] * g.quantidadeVertices
+
+    licacoes_validas = [0] * g.quantidadeVertices
     for i in range(g.quantidadeVertices):
-        licacoesValidas[i] = graphLibrary.grau(g, g.N[i]) 
+        licacoes_validas[i] = graphLibrary.grau(g, g.N[i])
 
     while (mst.quantidadeVertices != g.quantidadeVertices):
+
         arestasDaFilaPrincipal = {}
         FilaPrincipal = PriorityQueue()
-        for Vertex in range(mst.quantidadeVertices):
-            
-            idx = Vertex
-
-            if (licacoesValidas[Vertex] == 0):
+        for Vertex in mst.N:
+            idx = g.N.index(Vertex)
+            if (licacoes_validas[idx] == 0):
                 continue
-            for i in range( Vertex, len(g.M[Vertex]) ):
-                if (matrizDoGrafo[Vertex][i] > 0): # encontrei um vertice fora da arvore
-                    v = g.N[i] # Possivel novo vertice 
+            for i in range( idx, len(g.M[idx]) ):
+                if (matrizDoGrafo[idx][i] > 0): # encontrei um vertice fora da arvore
+                    v = g.N[i] # Possivel novo vertice
                     if not (mst.existe_vertice(v)):
-                        pesoArestaEncontrada = g.Mfila[Vertex][i].seeFist()
-                        arestaEncontrada = g.N[Vertex] + '-' + g.N[i]
+                        pesoArestaEncontrada = g.Mfila[idx][i].seeFist()
+                        arestaEncontrada = g.N[idx] + '-' + g.N[i]
                         arestasDaFilaPrincipal[pesoArestaEncontrada] = arestaEncontrada
                         FilaPrincipal.insert(pesoArestaEncontrada)
-        
-            idxfixo = idx 
+
+            idxfixo = idx
             while ( (idx- 1) >= 0 ):
                 if (matrizDoGrafo[idx-1][idxfixo] > 0):
-                    v = g.N[idx-1] # Possivel novo vertice 
+                    v = g.N[idx-1] # Possivel novo vertice
                     if not (mst.existe_vertice(v)):
                         pesoArestaEncontrada = g.Mfila[idx-1][idx].seeFist()
                         arestaEncontrada = g.N[idx-1] + '-' + g.N[idxfixo]
                         arestasDaFilaPrincipal[pesoArestaEncontrada] = arestaEncontrada
                         FilaPrincipal.insert(pesoArestaEncontrada)
                 idx -= 1
-        
+
         menorPesoAresta = FilaPrincipal.remove()
-        novoVertice = arestasDaFilaPrincipal[menorPesoAresta][-1]
+        vi = arestasDaFilaPrincipal[menorPesoAresta][0]
+        vf = arestasDaFilaPrincipal[menorPesoAresta][-1]
+        if not mst.existe_vertice(vi):
+            novoVertice = vi
+        else:
+            novoVertice = vf
         novaAresta = arestasDaFilaPrincipal[menorPesoAresta]
-        idxV1 = g.N.index(novaAresta[0])
-        idxV2 = g.N.index(novaAresta[-1])
         mst.adiciona_vertice(novoVertice)
         mst.adiciona_aresta(novaAresta, menorPesoAresta)
-        licacoesValidas[idxV1] -= 1
-        licacoesValidas[idxV2] -= 1
+        idxV1 = g.N.index(vi)
+        idxV2 = g.N.index(vf)
+        licacoes_validas[idxV1] -= 1
+        licacoes_validas[idxV2] -= 1
         matrizDoGrafo[idxV1][idxV2] -= 1
 
     return mst
 
-
-        
-
-        
-
-            
 def main():
 
     grafo = Grafo([], [])
     
     for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
         grafo.adiciona_vertice(i)
-    for i in [('J-C', 3), ('C-E', 1), ('C-P', 5), ('C-M', 4), ('C-T',2), ('M-T', 3), ('P-M', 1), ('T-Z', 6), ('M-Z', 9)]:
-        grafo.adiciona_aresta( *i )
+    for i in [('J-C', 3), ('C-E', 1), ('C-P', 5), ('C-M', 4), ('C-T', 2), ('M-T', 3), ('P-M', 1), ('T-Z', 6), ('M-Z', 9)]:
+        grafo.adiciona_aresta(*i)
 
     print("\nOriginal\n")
     print(grafo)
     print()
     print("Minimal Spanning Tree\n")
-    print(Prim(grafo))
-    
+    mst = Prim(grafo)
+    print(mst)    
     
 main()
